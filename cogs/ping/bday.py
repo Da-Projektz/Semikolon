d591d77460a84be4bc2145dddc92e43905ca81fc
jauniew21 <jauniew21@gmail.com>
2022-06-18 21:48:10 -0600

    bday functionality

--- cogs/ping/bday.py
+++ cogs/ping/bday.py
@@ -1,6 +1,14 @@
 from nextcord.ext import commands
 
-class Ping(commands.Cog, name="Bday"):
+class Bday(commands.Cog, name="bday"):
     def __init__(self, bot: commands.Bot):
         self._bot = bot
+    
+    #Someone says happy birthday?
+    @commands.Cog.listener()
+    async def on_message(message):
+        if message.contains == "birthday":
+            await message.channel.send('¡you old hag!')
 
+def setup(bot: commands.Bot):
+    bot.add_cog(Bday(bot))
